bad1210513659348c544d39f79b0db43315d7414
gearoffortune <hopelessuntildeath@gmail.com>
2021-03-27 19:11:50 +0300

    not clear which timezone is in activity; try UTC

--- activityData.ts
+++ activityData.ts
@@ -3,11 +3,11 @@ import {Commit, Sprint} from './examples/types'
 
 export function getActivityData(currentSprint: Sprint, currentCommits: Commit[]): ActivityData {
   const commitsByDay = [0, 1, 2, 3, 4, 5, 6].map(number => {
-    return currentCommits.filter(commit => (new Date(commit.timestamp)).getDay() === number)
+    return currentCommits.filter(commit => (new Date(commit.timestamp)).getUTCDay() === number)
   })
   const numberOfCommitsByHour = commitsByDay.map(commits => {
     return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map(hour => {
-      return commits.filter(commit => (new Date(commit.timestamp)).getHours() === hour).length
+      return commits.filter(commit => (new Date(commit.timestamp)).getUTCHours() === hour).length
     })
   })
   return {
