adbc9620d3190a43c0a2578b13d84b3760363f24
gearoffortune <hopelessuntildeath@gmail.com>
2021-03-27 15:42:45 +0300

    to be refactored

---
+++ activityData.ts
@@ -0,0 +1,26 @@
+import { ActivityData } from './examples/stories';
+import {Commit, Sprint} from './examples/types'
+
+export function getActivityData(currentSprint: Sprint, currentCommits: Commit[]): ActivityData {
+  const commitsByDay = [0, 1, 2, 3, 4, 5, 6].map(number => {
+    return currentCommits.filter(commit => (new Date(commit.timestamp)).getDay() === number)
+  })
+  const numberOfCommitsByHour = commitsByDay.map(commits => {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map(hour => {
+      return commits.filter(commit => (new Date(commit.timestamp)).getHours() === hour).length
+    })
+  })
+  return {
+    title: "Коммиты, 1 неделя",
+    subtitle: `Спринт № ${currentSprint.id}`,
+    data: {
+      mon: numberOfCommitsByHour[0],
+      tue: numberOfCommitsByHour[1],
+      wed: numberOfCommitsByHour[2],
+      thu: numberOfCommitsByHour[3],
+      fri: numberOfCommitsByHour[4],
+      sat: numberOfCommitsByHour[5],
+      sun: numberOfCommitsByHour[6],
+    }
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  // Where webpack looks to start building the bundle
+  entry: [path.resolve(__dirname, 'main.ts')],
+
+  // Where webpack outputs the assets and bundles
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: '[name].js',
+    library: {
+      name: 'PrepareData',
+      type: 'umd',
+    }
+
+  },
+
+  // Customize the webpack build process
+  plugins: [
+    // Removes/cleans build folders and unused assets when rebuilding
+    // new CleanWebpackPlugin(),
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      // JavaScript: Use Babel to transpile JavaScript files
+      // { test: /\.js$/, use: ['babel-loader'] },
+      {test: /\.ts$/, use: ['ts-loader']},
+    ],
+  },
+}
